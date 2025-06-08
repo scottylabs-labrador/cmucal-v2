1fc8ad7b102597a58e6d8db7908990d6cf7ef76e
qianxuege <qianxuege15@gmail.com>
2025-06-07 20:10:04 -0400

    removed unnecessary code

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,9 +1,4 @@
-from flask import current_app
 # app/models/user.py
-from sqlalchemy import Column, String, DateTime, BigInteger
-from datetime import datetime
-from app.services.db import Base, SessionLocal
-from flask import request, jsonify
 from app.models.models import User  
 
 
@@ -19,9 +14,6 @@ def user_to_dict(user):
     }
 
 def create_user(db, clerk_id, **kwargs):
-    # db = SessionLocal()
-    # user = db.query(User).filter(User.clerk_id == clerk_id).first()
-    # if not user:
     user = User(clerk_id=clerk_id, **kwargs)
     db.add(user)
     db.commit()
@@ -30,16 +22,6 @@ def create_user(db, clerk_id, **kwargs):
 
 def get_user_by_clerk_id(db, clerk_id):
     return db.query(User).filter(User.clerk_id == clerk_id).first()
-    # user = db.query(User).filter(User.clerk_id == clerk_id).first()
-    # if user:
-    #     return jsonify({
-    #         "id": str(user.id),
-    #         "email": user.email,
-    #         "first_name": user.first_name,
-    #         "last_name": user.last_name
-    #     })
-    # else:
-    #     return jsonify({"error": "User not found"}), 404
 
 
 def update_user_calendar_id(db, clerk_id, calendar_id):
@@ -52,21 +34,3 @@ def update_user_calendar_id(db, clerk_id, calendar_id):
     db.refresh(user)
     return user
 
-# def get_user_by_clerk_id(clerk_id: str):
-#     db = get_db()
-#     users_collection = db["users"]
-#     return users_collection.find_one({"clerk_id": clerk_id})
-
-# def create_user(clerk_id: str, email: str, first_name: str = "", last_name: str = ""):
-#     db = get_db()
-#     users_collection = db["users"]
-    
-#     user_data = {
-#         "clerk_id": clerk_id,
-#         "email": email,
-#         "first_name": first_name,
-#         "last_name": last_name,
-#     }
-    
-#     users_collection.insert_one(user_data)
-#     return user_data
