ca384156e34cc6df9c250ec8e192661b278fc626
HeidiTao <heidi_tao@outlook.com>
2025-08-14 20:34:52 +0800

    Add endpoints for fetching tags and updating event; fix endpoint for get_all_events to include filter conditions

--- backend/app/models/event_tag.py
+++ backend/app/models/event_tag.py
@@ -1,4 +1,5 @@
 from app.models.models import EventTag
+from app.models.models import Tag
 
 def save_event_tag(db, event_id: int, tag_id: int):
     """
@@ -20,3 +21,18 @@ def save_event_tag(db, event_id: int, tag_id: int):
     db.commit()
     db.refresh(event_tag)
     return event_tag
+
+def get_tags_by_event(db, event_id: int):
+    tags = (
+                db.query(Tag.id, Tag.name)
+                .join(Tag.event_tags) # relationship set up in models
+                # .join(EventTag, Tag.id == EventTag.tag_id)  # explicit join condition
+                .filter(EventTag.event_id == event_id)      # filter by the event id
+                .all()
+            )
+    return tags
+
+def delete_event_tag(db, event_id, tag_id):
+    db.query(EventTag).filter_by(event_id=event_id, tag_id=tag_id).delete()
+    print("🚮 deleting event tag", event_id, tag_id)
+    # db.commit()
